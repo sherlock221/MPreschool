9d5eba9f2899296b7af04d3e577a75805c5c95f9
abjia <daohao1914>
2015-06-05 14:26:37 +0800

    添加bootstrap

--- js/app.js
+++ js/app.js
@@ -9,5 +9,9 @@ var MPreschool = angular.module('MPreschool', [
     'ui.router',
     'ngAnimate',
     'ui.bootstrap',
-    'angular-loading-bar'
+    'angular-loading-bar',
+    "focusOn",
+    "toastr",
+    "ngFileUpload",
+    'mwl.confirm'
 ]);
